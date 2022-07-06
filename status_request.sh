20b092672579b1257d2bc54d147432cfcf718d06
Praveen Selvaraj <praveen.beatle@gmail.com>
2022-07-06 16:00:15 +0100

    curl reqs

---
+++ status_request.sh
@@ -0,0 +1,1 @@
+curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET localhost:5000/status
---
+++ toggle_request.sh
@@ -0,0 +1,1 @@
+curl -i -X POST -H 'Content-Type: application/json' -d '{"topic": "cmnd/localbytes_plug/Power", "msg": "toggle"}' localhost:5000/toggle
