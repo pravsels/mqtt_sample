ad1c7999308ddc60be73d5c54e15c0bceea4cc76
Praveen Selvaraj <praveen.beatle@gmail.com>
2022-07-06 13:01:46 +0100

    audio streaming nogo

--- audio_steaming/machine2.py
+++ audio_steaming/machine2.py
@@ -1,31 +1,23 @@
-
+#!/usr/bin/env python3
 from random import randint
 
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
 import pyaudio
-import numpy as np
-import soundcard as sc
-
 
 class Client(DatagramProtocol):
     def startProtocol(self):
         py_audio = pyaudio.PyAudio()
         self.buffer = 1024  # 127.0.0.1
-        # self.another_client = "127.0.0.1", int(3001)
-        self.another_client = "192.168.1.6", int(3000)
+        self.another_client = "192.168.1.7", int(3000)
 
-        # self.output_stream = py_audio.open(format=pyaudio.paInt16,
-        #                                    output=True, rate=44100, channels=2,
-        #                                    frames_per_buffer=self.buffer)
+        self.output_stream = py_audio.open(format=pyaudio.paInt16,
+                                           output=True, rate=44100, channels=2,
+                                           frames_per_buffer=self.buffer)
         self.input_stream = py_audio.open(format=pyaudio.paInt16,
                                           input=True, rate=44100, channels=2,
                                           frames_per_buffer=self.buffer)
-
-        self.default_speaker = sc.default_speaker()
-
         reactor.callInThread(self.record)
-
 
     def record(self):
         while True:
@@ -33,8 +25,9 @@ class Client(DatagramProtocol):
             self.transport.write(data, self.another_client)
 
     def datagramReceived(self, datagram, addr):
-        # self.output_stream.write(datagram)
-        self.default_speaker.play(datagram/np.max(datagram), samplerate=44100)
+        print('received audio datagram : ')
+        self.output_stream.write(datagram)
+
 
 if __name__ == '__main__':
     port = 3000
